975e723170c7c3fab04ea2861608850acb46bddb
Danik2225 <dand39114@gmail.com>
2026-08-06 18:18:21 +0400

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const multer = require("multer");
 const db = require("./database");
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
